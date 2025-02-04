cf3426ad01717672b0f866d5bab36244dd5716b7
David Garcia <davidsgarcia999@gmail.com>
2025-02-03 18:08:13 -0700

    finished server folder, adding to client

--- Develop/client/src/App.tsx
+++ Develop/client/src/App.tsx
@@ -1,15 +1,17 @@
-import './App.css';
-import { Outlet } from 'react-router-dom';
-
-import Navbar from './components/Navbar';
-
-function App() {
-  return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
-  );
+body {
+  font-family: 'Rubik', sans-serif;
 }
 
-export default App;
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-family: 'Rubik Mono One', sans-serif;
+}
+
+.small {
+  font-weight: 300;
+  font-style: italic;
+}
